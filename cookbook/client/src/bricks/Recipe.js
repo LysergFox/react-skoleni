b3bb8f821b122f346b5fd287f41fe438f8c2b5e9
Jan Vocetka <hopi6570@gmail.com>
2023-11-12 21:54:11 +0100

    8 lekce

--- cookbook/client/src/bricks/Recipe.js
+++ cookbook/client/src/bricks/Recipe.js
@@ -19,36 +19,54 @@ function Recipe(props) {
         setShowEditModal(true);
     };
 
-    return (
-        <Card>
-            <Card.Img variant={"top"} src={props.recipe.imgUri} />
-            <Card.Body>
-                <div>
-                    <Icon path={mdiChefHat} size={1} color={"grey"} />{" "}
-                    {props.recipe.name}
-                </div>
-                <div>
-                    <Icon path={mdiText} size={1} color={"grey"} />{" "}
-                    {props.recipe.description}
-                </div>
-                <div>
-                    <IngredientList
-                        ingredientList={props.recipe.ingredients}
-                        ingredientsList={props.ingredientsList}
-                    />
-                </div>
-                <Button onClick={handleEditClick}>Upravit recept</Button>
+    if (props.viewType === "grid-small")
+        return (
+            <Card>
+                <Card.Img variant={"top"} src={props.recipe.imgUri}/>
+                <Card.Body>
+                    <div>
+                        <Icon path={mdiChefHat} size={1} color={"grey"}/>{" "}
+                        {props.recipe.name}
+                    </div>
+                    <div>
+                        <Icon path={mdiText} size={1} color={"grey"}/>{" "}
+                        {props.recipe.description}
+                    </div>
+                </Card.Body>
+            </Card>
+        );
+    else if (props.viewType === "grid") {
+        return (
+            <Card>
+                <Card.Img variant={"top"} src={props.recipe.imgUri}/>
+                <Card.Body>
+                    <div>
+                        <Icon path={mdiChefHat} size={1} color={"grey"}/>{" "}
+                        {props.recipe.name}
+                    </div>
+                    <div>
+                        <Icon path={mdiText} size={1} color={"grey"}/>{" "}
+                        {props.recipe.description}
+                    </div>
+                    <div>
+                        <IngredientList
+                            ingredientList={props.recipe.ingredients}
+                            ingredientsList={props.ingredientsList}
+                        />
+                    </div>
+                    <Button onClick={handleEditClick}>Upravit recept</Button>
 
-                <RecipeForm
-                    recipe={props.recipe}
-                    showModal={showEditModal}
-                    setShowModal={setShowEditModal}
-                    ingredientList={props.ingredientsList}
-                    handleOpenEditModal={handleOpenEditModal}
-                />
-            </Card.Body>
-        </Card>
-    );
+                    <RecipeForm
+                        recipe={props.recipe}
+                        showModal={showEditModal}
+                        setShowModal={setShowEditModal}
+                        ingredientList={props.ingredientsList}
+                        handleOpenEditModal={handleOpenEditModal}
+                    />
+                </Card.Body>
+            </Card>
+        );
+    }
 }
 
 export default Recipe;
